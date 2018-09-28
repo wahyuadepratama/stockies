36884a71cca06e83a32e0e8d40960d40f3393b1c
wahyuadepratama <1511521024@student.unand.ac.id>
2018-09-28 07:30:32 +0700

    revisi v1.6

--- resources/views/mail/order-photo.blade.php
+++ resources/views/mail/order-photo.blade.php
@@ -1,39 +1,113 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-	<head>
+<head>
 
-	</head>
-	<body>
 
-	<div class="fh5co-loader"></div>
-	<div id="page">
+	<title>Stockies</title>
 
-    <div id="fh5co-product">
-  		<div class="container">
-        <h4>Terima kasih sudah mengunjungi <b>Stockies</b></h4>
-        <p>Kami telah menerima pembayaran dari transaksi anda. Berikut adalah daftar foto yang telah anda pesan:</p>
-        <table style="margin-left: 5%">
-          @foreach($cart as $data)
-          <tr>
-            <td>Judul</td>
-            <td>:</td>
-            <td>{{ $data->photo->nama }}</td>
-          </tr>
-          <tr>
-            <td>Ukuran</td>
-            <td>:</td>
-            <td>{{ $data->ukuran }}</td>
-          </tr>
-          <tr>
-            <td>Harga</td>
-            <td>:</td>
-            <td>Rp.{{ $data->price }}</td>
-          </tr><br>
-          @endforeach
-        </table>
-        <p>Kami sudah melampirkan foto yang anda pesan pada email ini. Terima kasih sudah membeli foto di <b>Stockies.</b></p>
-      </div>
-    </div>
+	<link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
 
-  </body>
+	<link href="https://fonts.googleapis.com/css?family=Istok+Web" rel="stylesheet">
+
+	<style type="text/css">
+		body {
+			background-color: white;
+			font-family: "Asap", sans-serif;
+			color: #828282;
+			margin: 0;
+			padding: 0;
+		}
+
+		.kotak {
+			width: 60%;
+			background-color: rgba(191,139,22, .5);
+			padding: 0 4% 4% 4%;
+			margin: 5% auto;
+		}
+
+		.kotak img{
+			padding-top: 4%;
+		}
+
+		.kotak h3{
+			color: #bf8b16;
+			font-size: 28px;
+			text-align: center;
+		}
+
+		.kotak .text {
+			text-align: justify;
+		}
+
+		.kotak .text a{
+			text-decoration: none;
+			color: #bf8b16;
+		}
+
+		.footer {
+			background-color: black;
+			padding: unset;
+
+		}
+
+		.footer .text {
+			font-size: 25px;
+			text-align: center;
+			color: white;
+			word-spacing: 3px;
+			letter-spacing: 1px;
+		}
+
+
+	</style>
+
+</head>
+
+
+<body>
+
+
+
+
+		<div class="kotak">
+
+				<img src="https://www.stockies.id/storage/images/Logo%20Fix.png">
+
+						<h3>Terimakasih Sudah Membeli Foto di Stockies</h3>
+
+									<div class="text" style="padding-bottom: 5%; ">
+										<div style="text-align: justify;">
+											Kami telah menerima pembayaran dari transaksi anda. Berikut adalah daftar foto yang telah anda pesan :
+										</div>
+
+										<div style="padding-top: 3%; text-align: left; margin-left: 10%;">
+
+											<table width="75%">
+												@foreach($cart as $data)
+												<tr>
+													<th>Judul</th>
+													<th width="15%">:</th>
+													<th>{{ $data->photo->nama }}</th>
+												</tr>
+												<tr>
+													<th>Ukuran</th>
+													<th width="15%">:</th>
+													<th>{{ $data->ukuran }}</th>
+												</tr>
+												<tr>
+													<th>Harga</th>
+													<th width="15%">:</th>
+													<th>{{ $data->price }}</th>
+												</tr>
+												@endforeach
+											</table>
+										</div>
+
+										<div style="padding-top: 5%; text-align: justify;">
+											Kami sudah melampirkan foto yang anda pesan pada email ini. Terimakasih sudah membeli foto di <a href="">Stockies</a>.
+										</div>
+									</div>
+		</div>
+
+</body>
 </html>
